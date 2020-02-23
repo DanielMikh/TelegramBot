f2daf8b3c653b00f21f2b8830c03fbaa6627adda
Danil Mikhailenko <danmm-1998@mail.ru>
2020-02-23 18:49:34 +0300

    Second pushing

--- User.py
+++ User.py
@@ -2,3 +2,6 @@ class User():
     def __init__(self, message):
         self.id = message.from_user.id
         self.username = str(message.from_user.username)
+    
+    def get_commit(self):
+        pass

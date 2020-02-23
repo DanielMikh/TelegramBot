76378ed50c2f8eae3826d35281657f2a98250238
DanielMikh <60117718+DanielMikh@users.noreply.github.com>
2020-02-23 18:51:26 +0300

    Delete User.py

--- User.py
+++
@@ -1,7 +0,0 @@
-class User():
-    def __init__(self, message):
-        self.id = message.from_user.id
-        self.username = str(message.from_user.username)
-    
-    def get_commit(self):
-        pass
